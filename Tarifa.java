b859361bfb623da18a0fe3d63ca1bcfcf6f91093
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-01-11 21:02:50 -0500

    Hello World

---
+++ Tarifa.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Tarifa {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int x = scan.nextInt();
+    int n = scan.nextInt();
+    int p = scan.nextInt();
+
+    for (int i = 100; i  )
+
+
+    scan.close();
+  }
+}
