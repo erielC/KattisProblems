0c4476a53ec4b447a5e4c3349464dc22e053fbb1
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-01-11 21:17:12 -0500

    Triangle Area

---
+++ triangleArea.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class triangleArea {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    double base = scan.nextDouble();
+    double height = scan.nextDouble();
+
+    System.out.println((base * height)/ 2);
+
+    scan.close();
+
+  }
+}
