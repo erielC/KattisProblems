2a5403e79b0aef7d41316a41a1d6e8231da61428
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-01-12 12:04:28 -0500

    Take Two Stones

---
+++ takeTwoStones.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class takeTwoStones {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int num = scan.nextInt();
+
+    if(num % 2 == 0 ){
+      System.out.println("Bob");
+    } else {
+      System.out.println("Alice");
+
+    }
+
+    
+    scan.close();
+  
+  }
+}
