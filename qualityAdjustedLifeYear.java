6dcdd7444e36a33fd47ab1b308c958ef129dc1b8
Eriel Cabrera <98791243+erielC@users.noreply.github.com>
2023-01-13 09:05:21 -0500

    Quality Adjusted Life Year

--- qualityAdjustedLifeYear.java
+++ qualityAdjustedLifeYear.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class qualityAdjustedLifeYear {
   public static void main(String[] args) {
   Scanner scan = new Scanner(System.in);
+
+    // int periodOfTime 
+    // int qualityOfLife 
+    // int numOfYears 
+
     
   scan.close();
   }
