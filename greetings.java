e40e40d4626c093d3a9f345104fb8c953dc7713b
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-01-17 12:28:19 -0500

    Greetings!

---
+++ greetings.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class greetings {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    // hey (letter E is on the 1 index number)
+    String text = scan.nextLine();
+    int n = (text.length() * 2) - 4;
+
+    System.out.print("h");
+    for (int i = 0; i < n; i++){
+      System.out.print("e");
+    }
+    
+    System.out.print("y");
+    
+  }
+}
