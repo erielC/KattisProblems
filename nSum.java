34afaea9dc3bc5715a1a1f37c6785b161ec442f4
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-01-17 11:31:23 -0500

    N-sum

---
+++ nSum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class nSum {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int i, numX;
+    int total = 0;
+     
+
+    int n = scan.nextInt();
+
+    for (i = 0; i < n; i++) {
+      numX = scan.nextInt();
+      total += numX;
+    }
+    
+    System.out.println(total);
+    scan.close();
+  }
+}
