0ddefe0b8afdd89ebf4ab87a2d036dd920de49ef
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-01-17 12:28:42 -0500

    Greetings!

--- greetings.java
+++ greetings.java
@@ -6,7 +6,8 @@ public class greetings {
     // hey (letter E is on the 1 index number)
     String text = scan.nextLine();
     int n = (text.length() * 2) - 4;
-
+    scan.close();
+    
     System.out.print("h");
     for (int i = 0; i < n; i++){
       System.out.print("e");
