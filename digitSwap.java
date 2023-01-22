de2da628a4f1ed7444dcb93d795f1efde88b9a65
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-01-22 18:02:22 -0500

    Digit Swap

--- digitSwap.java
+++ digitSwap.java
@@ -5,6 +5,17 @@ public class digitSwap {
     Scanner scan = new Scanner(System.in);
 
     int x = scan.nextInt(); 
+    scan.close();
+
+    String conversion = Integer.toString(x);
+
+
+    System.out.print(conversion.substring(1,2));
+
+    System.out.print(conversion.substring(0,1));
+
+    
+
   
    
   }
