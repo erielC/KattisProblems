e44ddb654d43b2a7f2d5d1880a8be350cc17d4dd
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-01-22 18:07:26 -0500

    Solving For Carrots

---
+++ solvingForCarrots.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class solvingForCarrots {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int numOfContestants = scan.nextInt();
+    int numOfCarrots = scan.nextInt();
+    scan.close();
+
+    System.out.println(numOfCarrots);
+  }
+}
