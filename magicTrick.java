695fc2b2364ca339cc5b619af825a1b7d9790dad
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-01-26 11:45:00 -0500

    Update magicTrick.java

--- magicTrick.java
+++ magicTrick.java
@@ -2,23 +2,19 @@ import java.util.Scanner;
 
 public class magicTrick {
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    x = 0;
-    String intialArrangemnet = scan.nextLine();
-    int length = intialArrangemnet.length();
+      Scanner scan = new Scanner(System.in);
+   
+    String word = scan.nextLine();
+    int ans = 1;
 
-    for(int i = 0; i < length; i++){
-      System.out.println(intialArrangemnet.substring(i));
-      for(int j = i + 1; i < length;) {
-        if (intialArrangemnet.substring(i) == intialArrangemnet.substring(j)) {
-          int x = 1;
-        } else {
-          int x = 0;
-        }
+    for (int i = 0; i < word.length(); i++) {
+      for (int j = i + 1; j < word.length(); j++) {
+       if (word.substring(i, i + 1).equals(word.substring(j, j +1))){
+        ans = 0;
+       }
+       
       }
-      
     }
-    
-    System.out.println(x);
+    System.out.println(ans);
   }
 }
