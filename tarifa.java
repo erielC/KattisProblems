2a31b23c005ed012181bad38e8b62669c617894b
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-01-31 18:44:13 -0500

    not done

--- tarifa.java
+++ tarifa.java
@@ -3,21 +3,24 @@ import java.util.Scanner;
 public class tarifa {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-
     // x = megabytes allowed to use by provides
     // y = how many months have passed
     // n = are the test cases that follow
     int x = scan.nextInt();
     int y = scan.nextInt();
-    int actualTotal = 0;
 
-    for (int i = 0; i < y; i++) {
+    int firstCase = scan.nextInt();
+    int workWith = x - firstCase;
+
+    int added = x + workWith;
+ 
+    for(int i = 0; i < (y-1); i++) {
       int n = scan.nextInt();
-      int remainder = 10-8; 
-      int total = x + remainder;
-      actualTotal = total - 10;
+      x += added - n;
     }
-    System.out.println(actualTotal);
     scan.close();
+    int total = 0;
+  
+    System.out.println(total);
   }
 }
