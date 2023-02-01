f1acf7387f0a124286353b8747b2da24e9fe4b68
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-02-01 16:36:09 -0500

    not done

---
+++ detailedDifferences.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class detailedDifferences {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int testcase = scan.nextInt(); 
+
+    for (int i = 0; i < testcase ; i++){
+      String x = scan.nextLine();
+      String y = scan.nextLine();
+    }
+    
+    scan.close();
+  }
+}
