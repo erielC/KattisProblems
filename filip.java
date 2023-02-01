0229124e29243b181d02ee7086f5c389eee05980
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-02-01 16:21:34 -0500

    Filip

--- filip.java
+++ filip.java
@@ -1,21 +1,44 @@
 import java.util.Scanner;
 
 public class filip {
+
+  static int reverseA (int a) {
+    
+    int rev = 0;
+    int rem; 
+    while (a > 0) {
+      rem = a%10;
+      rev = (rev*10) + rem;
+      a = a/10;
+    }
+    return rev;
+  }
+
+  static int reverseB (int b) {
+    
+    int rev = 0;
+    int rem; 
+    while (b > 0) {
+      rem = b%10;
+      rev = (rev*10) + rem;
+      b = b/10;
+    }
+    return rev;
+  }
+
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-
     int a = scan.nextInt();
     int b = scan.nextInt();
 
-    String convertedA = Integer.toString(a);
+    if (reverseA(a) > reverseB(b)) {
+      System.out.println(reverseA(a));
+    } else {
+      System.out.println(reverseB(b));
+    }
 
-    System.out.println(convertedA);
-    System.out.println("-------------");
 
-    System.out.print(convertedA.substring(2));
-    System.out.print(convertedA.substring(1));
    
-
     scan.close();
   }
 }
