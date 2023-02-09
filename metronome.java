51d0a672b6a23f29baf755293d7c098494e38c08
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-02-09 12:14:30 -0500

    Metronome

---
+++ metronome.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Formatter;
+
+public class metronome {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    Formatter dec = new Formatter();
+
+    double number = scan.nextInt();
+
+    System.out.println(dec.format("%.2f", (number/4)));
+
+    scan.close();
+  }
+}
