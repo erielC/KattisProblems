bb4bef66517486c787bbea2470bc1787bacef301
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-03-09 06:45:18 -0500

    Create unitSix.java

---
+++ unitSix.java
@@ -0,0 +1,13 @@
+public class unitSix {
+  public static void main(String[] args) {
+    
+  String[] words = {"ten", "fading", "post", "card", "thunder", "hinge", "trailing", "batting"};
+
+for (String word : words) {
+    if (word.endsWith("ing")) {
+        System.out.println(word);
+    }
+}
+
+}
+}
