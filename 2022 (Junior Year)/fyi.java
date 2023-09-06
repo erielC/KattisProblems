c0f2646cf745c789ad045b97c5b749d13c7fcf53
Eriel Cabrera <98791243+erielC@users.noreply.github.com>
2023-09-05 21:48:24 -0400

    fyi.java

--- 2022 (Junior Year)/fyi.java
+++ 2022 (Junior Year)/fyi.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class fyi {
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in); 
 
     int num = scan.nextInt();
     scan.close();
