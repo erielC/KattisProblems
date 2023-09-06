4b6595021b56a4b2c8ec83ddd5f24f1cf6255554
Eriel Cabrera <98791243+erielC@users.noreply.github.com>
2023-09-05 21:50:22 -0400

    hungry.java

--- 2022 (Junior Year)/hungry.java
+++ 2022 (Junior Year)/hungry.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class hungry {
   
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in); 
     ArrayList<Integer> arr = new ArrayList<Integer>();
 
     int totalMoney = scan.nextInt();
