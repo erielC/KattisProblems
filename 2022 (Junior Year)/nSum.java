31d8ad50e3122bb5cd62c5ca5d267b42a6013a35
Eriel Cabrera <98791243+erielC@users.noreply.github.com>
2023-09-05 21:51:38 -0400

    nSum.java

--- 2022 (Junior Year)/nSum.java
+++ 2022 (Junior Year)/nSum.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class nSum {
-  public static void main(String[] args) {
+  public static void main(String[] args) { 
     Scanner scan = new Scanner(System.in);
 
     int i, numX;
