c192e09eb8d72d7e66f2f490c9885f75f5888f83
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-09-05 21:32:55 -0400

    Create hungry.java

---
+++ hungry.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class hungry {
+  
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    ArrayList<Integer> arr = new ArrayList<Integer>();
+
+    int totalMoney = scan.nextInt();
+    int amtOfLoop = scan.nextInt();
+
+    int n = 0;
+    int x = 0;
+
+    for (int i = 0; i < amtOfLoop; i++) {
+      n = scan.nextInt();
+      x = scan.nextInt();
+
+
+    }
+
+  }
+}
