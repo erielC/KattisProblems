4f99f20b56fa0a426c602c6c769189bdc978d32d
Eriel Cabrera <98791243+erielC@users.noreply.github.com>
2023-09-05 21:47:57 -0400

    findingAnA.java

--- 2022 (Junior Year)/findingAnA.java
+++ 2022 (Junior Year)/findingAnA.java
@@ -2,7 +2,7 @@
  
  public class findingAnA {
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in); 
 
     String text = scan.nextLine();
     scan.close();
