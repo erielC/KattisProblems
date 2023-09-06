9299872626c1dc9148470ce5307953ede00959b4
Eriel Cabrera <98791243+erielC@users.noreply.github.com>
2023-09-05 21:52:30 -0400

    rTwo.java

--- 2022 (Junior Year)/rTwo.java
+++ 2022 (Junior Year)/rTwo.java
@@ -4,7 +4,7 @@ public class rTwo {
   public static void main(String[] args) {
    Scanner scan = new Scanner(System.in);
     
-   int a = scan.nextInt();
+   int a = scan.nextInt(); 
    int b = scan.nextInt();
 
    System.out.println(2 * b - a);
