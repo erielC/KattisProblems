4b946c2385895dea6224b21bd45493656c8e416b
Eriel Cabrera <98791243+erielC@users.noreply.github.com>
2023-09-05 21:51:28 -0400

    metronome.java

--- 2022 (Junior Year)/metronome.java
+++ 2022 (Junior Year)/metronome.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Formatter;
 
 public class metronome {
-  public static void main(String[] args) {
+  public static void main(String[] args) { 
     Scanner scan = new Scanner(System.in);
     Formatter dec = new Formatter();
 
